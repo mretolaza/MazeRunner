4034cb87e998e3dc459a491b28f976c53c530ddf
ret16339 <ret16339@uvg.edu.gt>
2017-05-02 07:53:58 -0600

    Programa Guia de Conexion a RED

---
+++ UNetworkManager/Assets/personaje.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class personaje : NetworkBehaviour {
+
+	int amount = 33;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!isLocalPlayer)
+			return;
+
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			GetComponent<Rigidbody>().AddForce(new Vector3(-amount,0,0));
+
+		}
+
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			GetComponent<Rigidbody>().AddForce(new Vector3(amount,0,0));
+		
+		}
+
+
+		
+	}
+}
